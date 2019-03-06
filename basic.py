5ae31e464648d49f6a00a46d01875d36d648fb83
Binh Nguyen <BNguyen@acutus.com>
2019-03-06 11:17:58 -0800

    fixed white spaces

--- basic.py
+++ basic.py
@@ -14,7 +14,7 @@ fashion_mnist = keras.datasets.fashion_mnist
 (train_images, train_labels), (test_images, test_labels) = fashion_mnist.load_data()
 
 class_names = ['T-shirt/top', 'Trouser', 'Pullover', 'Dress', 'Coat',
-			   'Sandal', 'Shirt', 'Sneaker', 'Bag', 'Ankle boot']
+               'Sandal', 'Shirt', 'Sneaker', 'Bag', 'Ankle boot']
 
 train_images.shape
 
@@ -40,25 +40,25 @@ test_images = test_images / 255.0
 
 plt.figure(figsize=(10,10))
 for i in range(25):
-	plt.subplot(5,5,i+1)
-	plt.xticks([])
-	plt.yticks([])
-	plt.grid(False)
-	plt.imshow(train_images[i], cmap=plt.cm.binary)
-	plt.xlabel(class_names[train_labels[i]])
+    plt.subplot(5,5,i+1)
+    plt.xticks([])
+    plt.yticks([])
+    plt.grid(False)
+    plt.imshow(train_images[i], cmap=plt.cm.binary)
+    plt.xlabel(class_names[train_labels[i]])
 plt.show(block = False)
 
 # Setup the layers
 model = keras.Sequential([
-	keras.layers.Flatten(input_shape=(28,28)),
-	keras.layers.Dense(128, activation=tf.nn.relu),
-	keras.layers.Dense(10, activation=tf.nn.softmax)
+    keras.layers.Flatten(input_shape=(28,28)),
+    keras.layers.Dense(128, activation=tf.nn.relu),
+    keras.layers.Dense(10, activation=tf.nn.softmax)
 ])
 
 # Compile the model
 model.compile(optimizer='adam',
-	loss='sparse_categorical_crossentropy',
-	metrics=['accuracy'])
+    loss='sparse_categorical_crossentropy',
+    metrics=['accuracy'])
 
 # Train the model
 model.fit(train_images, train_labels,epochs=5)
@@ -75,35 +75,35 @@ np.argmax(predictions[0])
 test_labels[0]
 
 def plot_image(i, predictions_array, true_label, img):
-	predictions_array, true_label, img = predictions_array[i], true_label[i], img[i]
-	plt.grid(False)
-	plt.xticks([])
-	plt.yticks([])
+    predictions_array, true_label, img = predictions_array[i], true_label[i], img[i]
+    plt.grid(False)
+    plt.xticks([])
+    plt.yticks([])
 
-	plt.imshow(img, cmap=plt.cm.binary)
+    plt.imshow(img, cmap=plt.cm.binary)
 
-	predicted_label = np.argmax(predictions_array)
-	if predicted_label == true_label:
-		color = 'blue'
-	else:
-		color = 'red'
+    predicted_label = np.argmax(predictions_array)
+    if predicted_label == true_label:
+        color = 'blue'
+    else:
+        color = 'red'
 
-	plt.xlabel("{} {:2.0f}% ({})".format(class_names[predicted_label],
-								  100*np.max(predictions_array),
-								  class_names[true_label]),
-								  color=color)
+    plt.xlabel("{} {:2.0f}% ({})".format(class_names[predicted_label],
+                                  100*np.max(predictions_array),
+                                  class_names[true_label]),
+                                  color=color)
 
 def plot_value_array(i, predictions_array, true_label):
-	predictions_array, true_label = predictions_array[i], true_label[i]
-	plt.grid(False)
-	plt.xticks([])
-	plt.yticks([])
-	thisplot = plt.bar(range(10), predictions_array, color="#777777")
-	plt.ylim([0, 1])
-	predicted_label = np.argmax(predictions_array)
+    predictions_array, true_label = predictions_array[i], true_label[i]
+    plt.grid(False)
+    plt.xticks([])
+    plt.yticks([])
+    thisplot = plt.bar(range(10), predictions_array, color="#777777")
+    plt.ylim([0, 1])
+    predicted_label = np.argmax(predictions_array)
 
-	thisplot[predicted_label].set_color('red')
-	thisplot[true_label].set_color('blue')
+    thisplot[predicted_label].set_color('red')
+    thisplot[true_label].set_color('blue')
 
 i = 0
 plt.figure(figsize=(6,3))
@@ -129,10 +129,10 @@ num_cols = 3
 num_images = num_rows*num_cols
 plt.figure(figsize=(2*2*num_cols, 2*num_rows))
 for i in range(num_images):
-	plt.subplot(num_rows, 2*num_cols, 2*i+1)
-	plot_image(i, predictions, test_labels, test_images)
-	plt.subplot(num_rows, 2*num_cols, 2*i+2)
-	plot_value_array(i, predictions, test_labels)
+    plt.subplot(num_rows, 2*num_cols, 2*i+1)
+    plot_image(i, predictions, test_labels, test_images)
+    plt.subplot(num_rows, 2*num_cols, 2*i+2)
+    plot_value_array(i, predictions, test_labels)
 
 # Grab an image from the test dataset
 img = test_images[0]
